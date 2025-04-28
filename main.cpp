369fc4a893d5d027af8e82863a4721a254ac4d36
thanhha24dt <hathanh24dt@gmail.com>
2025-04-28 20:48:17 +0700

    Tính Tích

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ int main() {
     // Tính tổng và hiệu
     int sum = a + b + c;
     int diff = a - b - c;
-
+    // Tính tích
+    int tich= a  *b *c;
     // In ra kết quả
     cout << "Tong: " << sum << endl;
     cout << "Hieu: " << diff << endl;
-
+    cout << "Tich: " << tich << endl;
     return 0;
 }
