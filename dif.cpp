475082a7947f59b8fae6064549b7474347a20c9c
ngocdiep2509 <diepchungoc259@gmail.com>
2025-04-28 20:42:47 +0700

    thanh vien

---
+++ dif.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Khai báo 3 số nguyên
+    int a = 5, b = 3, c = 2;
+
+    // Tính hiệu
+    int diff = a - b - c;
+
+    // In ra kết quả
+    cout << "Hieu: " << diff << endl;
+
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Khai báo 3 số nguyên
+    int a = 5, b = 3, c = 2;
+
+    // Tính tổng 
+    int sum = a + b + c;
+
+    // In ra kết quả
+    cout << "Tong: " << sum << endl;
+
+    return 0;
+}
